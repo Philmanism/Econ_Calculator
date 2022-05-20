961257520b013e008c4bc650948dc1727e6106d0
Philmanism <jacobphilman@gmail.com>
2022-05-19 19:27:53 -0600

    Update real_gdp_calc_gdp_deflator.py

--- real_gdp_calc_gdp_deflator.py
+++ real_gdp_calc_gdp_deflator.py
@@ -4,14 +4,16 @@ Created on Sat May 14 15:50:59 2022
 
 @author: maybe
 """
+#get nominal GDP from user and return to function
 def get_nom_gdp():
     nom_gdp = float(input("Enter Nominal GDP: "))
     return nom_gdp
+#get real gdp from user and return to function
 def get_real_gdp():
     real_gdp = float(input("Enter Real GDP: "))
     return real_gdp
                      
-
+#perform gdp deflator calculation and then recalculate real gdp (returns the same number as initially entered for real gdp)
 def real_gdp_calc():
     nom_gdp = get_nom_gdp()
     real_gdp = get_real_gdp()
